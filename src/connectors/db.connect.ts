6680fdf440d5df8a78a2a910682e78a52e4f7da2
aaronalayo <aaron.aa@me.com>
2022-12-05 14:22:25 +0100

    Merge branch 'main' of github.com:aaronalayo/priceit into ebayPagination

---
+++ src/connectors/db.connect.ts
@@ -0,0 +1,16 @@
+import mongoose, { mongo } from 'mongoose'
+import dotenv from 'dotenv'
+import { config } from '../config/config.js'
+dotenv.config();
+
+
+// mongo db connect
+mongoose.connect(config.mongo.url, { retryWrites: true, writeConcern: { w: "majority", j: true}})
+.then(() => {
+    console.log('Connected to DB ...')
+})
+.catch(error => {
+    console.log('Error connecting to DB: ' + error)
+})
+
+export default mongoose;
