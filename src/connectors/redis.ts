add3a7a6259247f55844494b3784b0e5105cef4a
aaronalayo <aaron.aa@me.com>
2022-12-07 02:04:08 +0100

    Added redis to pagination

--- src/connectors/redis.ts
+++ src/connectors/redis.ts
@@ -1,5 +1,6 @@
 import redis from "redis";
 import dotenv from 'dotenv'
+
 dotenv.config()
 export const redisClient = redis.createClient({
         url: process.env.REDIS_HOST,
@@ -16,3 +17,4 @@ export const redisClient = redis.createClient({
 
   await redisClient.ping();
 })();
+
