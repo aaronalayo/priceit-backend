90cad1bd098fd835d8c4953c632a3ee605a6d671
Zamanien <tari0048@stud.kea.dk>
2023-01-07 23:00:19 +0100

    fixed CORS issues

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,32 @@ import cors from 'cors';
 import * as dotenv from 'dotenv';
 import { Express, Request, Response } from 'express';
 import { router } from './routes/routes.js';
-
-dotenv.config();
-
 const app: Express = express();
 // app.use(cors); /* NEW */
+dotenv.config();
+app.use(express.json());
+
 
 import { config } from './config/config.js';
 import mongoose from './connectors/db.connect.js';
 
-dotenv.config();
-// mongo db connect
-mongoose;
-const allowedOrigins = ['http://localhost:5173'];
+// const allowedOrigins = ['http://127.0.0.1:5173/'];
+// const options: cors.CorsOptions = {
+//   origin: allowedOrigins,
+// };
+// app.use(cors(options));
 
-app.use(express.json());
-
-const options: cors.CorsOptions = {
-  origin: allowedOrigins,
-};
-app.use(cors(options));
+//Fixes CORS issues with ReactJS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); //
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(router);
 app.listen(config.server.local_port, () => {
   console.log(`⚡️[SEARCH_API]: Server is running at http://localhost:${config.server.local_port} | Docker PORT: ${config.server.docker_port}`);
+  mongoose;
 });
 
 // router.on('uncaughtException', (e)=> {
