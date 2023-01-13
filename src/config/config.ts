045fa913977960e4ba929fe7227515a44b043ac9
zamanien <tari0048@stud.kea.dk>
2023-01-13 16:38:40 +0100

    Some general cleanup and docker-compose fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,13 +8,13 @@ const MONGO_HOST = process.env.MONGO_HOST as string;
 const MONGO_PORT = process.env.MONGO_PORT as unknown as number;
 const MONGO_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}` as string;
 
-const REDIS_HOST = process.env.REDIS_HOST
+const REDIS_HOST = process.env.REDIS_HOST as string;
 const REDIS_PORT = process.env.REDIS_PORT as unknown as number;
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD as string;
 const REDIS_URL = process.env.REDIS_URL as string;
 
 const DOCKER_SERVER_PORT = process.env.DOCKER_SERVER_PORT as unknown as number || 8090;
-const PORT = process.env.PORT as unknown as number || 8080
+const PORT = process.env.PORT as unknown as number || 8080;
 
 export const config = {
   mongo: {
@@ -28,6 +28,6 @@ export const config = {
     host: REDIS_HOST,
     port: REDIS_PORT,
     password: REDIS_PASSWORD,
-    url: REDIS_URL
+    redis_url: REDIS_URL
   }
 };
--- src/connectors/redis.ts
+++ src/connectors/redis.ts
@@ -1,45 +1,19 @@
 import redis from 'redis';
 import { config } from '../config/config.js'
 
-// dotenv.config();
-// export const redisClient = redis.createClient({
-//   url: process.env.REDIS_HOST,
-//   password: process.env.REDIS_PASSWORD,
-// });
-
-// (async () => {
-//   redisClient.on('error', (err: any) => {
-//     console.log('Redis Client Error', err);
-//   });
-//   redisClient.on('ready', () => console.log('Redis is ready'));
-
-//   await redisClient.connect();
-
-//   await redisClient.ping();
-// })();
-
 const redisClient = redis.createClient({
   socket: {
     host: config.redis.host,
     port: config.redis.port
   },
-  password: config.redis.password
+  password: config.redis.password //Comment this out if you run locally
 });
 
 (async () => {
-  redisClient.on('error', (err: any) => {
+  redisClient.on('error', (err: any) => {    
     console.log('Redis Client Error', err);
   });
-  redisClient.on('ready', () => console.log('Redis is ready'));
-// const connectRedis = async () => {
-//   try {
-//     await redisClient.connect();
-//     console.log('Redis client connected...');
-//   } catch (err: any) {
-//     console.log(err.message);
-//     setTimeout(connectRedis, 5000);
-//   }
-// };
+  await redisClient.on('ready', () => console.log('Redis is ready'));
 
   await redisClient.connect();
 
